11adc54f2cb5be8fa075d46c449aefdf06501f0f
DusterTheFirst <DusterTheFirst@gmial.com>
2016-11-11 10:17:09 -0500

    more mobile friendly and feed0.0

--- res/private/feed.js
+++ res/private/feed.js
@@ -1,13 +1,51 @@
+var feeditm = null;
+$.ajax({
+    url : "/res/private/feed.html",
+    async: false,
+    success : function(result){
+        feeditm = result;
+    }
+});
 $.getJSON( "/feed.json", function( data ) {
+    //make list for data
     var li = [data.length];
+    //counter
     var count = 0;
+    //feeditem
+    var contain = document.createElement("div");
+    contain.innerHTML = feeditm;
+    //loops through all posts
     $.each( data, function(key, val) {
-        //var feeditem = window.getElementsByClassName("feeditem")[0];
-        //var itemtitle = feeditem.getElementsByClassName("title")[0];
-        li[count] = key;
+        var elms = {};
+        $.each(val, function(key, val){
+            elms[key] = val;
+        })
+        alert("notded");
+        var tempcontain = contain.cloneNode(true);
+        var link = tempcontain.getElementsByClassName("link")[0];
+        var title = tempcontain.getElementsByClassName("title")[0];
+        var desc = tempcontain.getElementsByClassName("desc")[0];
+        var content = tempcontain.getElementsByClassName("feedcontent")[0];
+        var date = tempcontain.getElementsByClassName("feedate")[0];
+        alert(tempcontain.innerHTML);
+        title.innerHTML = key;
+//        desc.innerHTML = elms["linkname"];
+//        content.innerHTML = elms["content"];
+//        date.innerHTML = elms["date"];
+//        link.href = elms["link"];
+        li[count] = tempcontain;
         count++;
+        alert(elms);
     });
     alert(li);
     
-    var feed = $("#feed");
+    var feed = document.getElementById("feed");
+    var htm = "";
+    for(var i = 0; i<5; i++){
+        if(li[i] != null){
+            htm += li[i].innerHTML;
+        }
+    }
+    feed.innerHTML = htm;
+    alert(htm);
 });
