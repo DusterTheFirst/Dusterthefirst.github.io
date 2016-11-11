d2632def0345920e5beeb64a4ca641ec4f237727
DusterTheFirst <DusterTheFirst@gmial.com>
2016-11-11 07:58:24 -0500

    MOble Support

--- res/private/feed.js
+++ res/private/feed.js
@@ -2,23 +2,12 @@ $.getJSON( "/feed.json", function( data ) {
     var li = [data.length];
     var count = 0;
     $.each( data, function(key, val) {
+        //var feeditem = window.getElementsByClassName("feeditem")[0];
+        //var itemtitle = feeditem.getElementsByClassName("title")[0];
         li[count] = key;
         count++;
     });
     alert(li);
     
     var feed = $("#feed");
-    /*Tablet Changes*/
-    if(document.documentElement.clientWidth < 768) {
-        feed.css("column-count", "2");
-        alert("2col");
-    /*Mobile Changes*/
-    }else if(document.documentElement.clientWidth < 426){
-        feed.css("column-count", "1");
-        alert("1col");
-    /*PC*/
-    }else{
-        feed.css("column-count", "3");
-        alert("3col");
-    }
 });
