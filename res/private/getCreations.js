dc433ee4c064f5b51e63f71394b47889a7888dda
DusterTheFirst <DusterTheFirst@gmial.com>
2016-12-13 17:38:11 -0500

    auto update

---
+++ res/private/getCreations.js
@@ -0,0 +1,34 @@
+ $.getJSON( "https://api.github.com/users/dusterthefirst/repos", function( data ) {
+  var items = "";
+  $.each( data, function( key, val ) {
+      var name, link, fullname, description, language, lastupdate, lic;
+      $.each(val, function(key, val){
+        switch(key) {
+            case "name":
+                name = val;
+                break;
+            case "full_name":
+                fullname = val;
+                break;
+            case "html_url":
+                link = val;
+                break;
+            case "description":
+                description = val;
+                break;
+            case "language":
+                language = val;
+                break;
+            case "updated_at":
+                lastupdate = val;
+                break;
+        }
+      });
+      language = description.split(" - ")[0];
+      description = description.split(" - ")[1];
+      if(language != "Game"){
+          items += ("<div class='feeditem half'><a class='link' href='" + link + "'><b><div class='title'>" + language + " - " + name + "</div></b><i><div class='desc'>" + fullname +"</div></i></a><hr><div class='feedcontent'>" + description + "</div><i><div class='feeddate'>" + lastupdate.split("T")[0] + "</div></i></div>");
+      }
+  });
+  document.getElementById("creations").innerHTML = items;
+});
