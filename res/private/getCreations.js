bc5e13c600d60c581e45d47bbc80e3d82492fa70
Zachary Kohnen <DusterTheFirst@users.noreply.github.com>
2017-07-06 02:29:41 -0400

    alpha

--- res/private/getCreations.js
+++ res/private/getCreations.js
@@ -1,34 +1,64 @@
  $.getJSON( "https://api.github.com/users/dusterthefirst/repos", function( data ) {
-  var items = "";
-  $.each( data, function( key, val ) {
-      var name, link, fullname, description, language, lastupdate, lic;
-      $.each(val, function(key, val){
-        switch(key) {
-            case "name":
-                name = val;
-                break;
-            case "full_name":
-                fullname = val;
-                break;
-            case "html_url":
-                link = val;
-                break;
-            case "description":
-                description = val;
-                break;
-            case "language":
-                language = val;
-                break;
-            case "updated_at":
-                lastupdate = val;
-                break;
+    var items = "";
+    $.each(data, function( key, val ) {
+        var name, link, fullname, description, language, lastupdate, lic;
+        $.each(val, function(key, val){
+            switch(key) {
+                case "name":
+                    name = val;
+                    break;
+                case "full_name":
+                    fullname = val;
+                    break;
+                case "html_url":
+                    link = val;
+                    break;
+                case "description":
+                    description = val;
+                    break;
+                case "language":
+                    language = val;
+                    break;
+                case "updated_at":
+                    lastupdate = val;
+                    break;
+            }
+        });
+        language = description.split(" - ")[0];
+        description = description.split(" - ")[1];
+        if(language != "Game"){
+            items += (`<div class='feeditem half'>
+                        <a class='link' href='${link}'>
+                            <b><div class='title'>
+                                ${language} - ${name}
+                            </div></b>
+                            <i><div class='desc'>
+                                ${fullname}
+                            </div></i>
+                        </a>
+                        <hr>
+                        <div class='feedcontent'>
+                            ${description}
+                        </div>
+                        <i><div class='feeddate'>
+                            ${lastupdate.split("T")[0]}
+                        </div></i>
+                    </div>`.replaceAll("    ", "").replaceAll("\n", ""));
         }
-      });
-      language = description.split(" - ")[0];
-      description = description.split(" - ")[1];
-      if(language != "Game"){
-          items += ("<div class='feeditem half'><a class='link' href='" + link + "'><b><div class='title'>" + language + " - " + name + "</div></b><i><div class='desc'>" + fullname +"</div></i></a><hr><div class='feedcontent'>" + description + "</div><i><div class='feeddate'>" + lastupdate.split("T")[0] + "</div></i></div>");
-      }
-  });
-  document.getElementById("creations").innerHTML = items;
+    });
+     document.getElementById("creations").innerHTML = items;
+     var childs = $("#creations").children();
+     childs = childs.sort(sortByName).toArray();
+     for (var i = 0; i < childs.length; i++) {
+        childs[i].parentNode.appendChild(childs[i]);
+     }
 });
+
+function sortByName(a, b) {
+    return $(a).find(".title").text().toLowerCase().localeCompare($(b).find(".title").text().toLowerCase());
+}
+
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.split(search).join(replacement);
+};
